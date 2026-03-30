f33ff9ea9290097feface0cd5fc005e6f57fb263
WilliamLokUM <dc32568@umac.mo>
2026-03-30 10:35:07 +0800

    commit

---
+++ CISC3003-Practice09-2026MAR27/header.inc.php
@@ -0,0 +1,23 @@
+<header class="mdl-layout__header">
+  <div class="mdl-layout__header-row">
+   <h1 class="mdl-layout-title"><span>CRM</span> Admin</h1>
+   <div class="mdl-layout-spacer"></div>
+    
+    <div class="mdl-navigation">
+      <label class="material-icons mdl-badge mdl-badge--overlap" data-badge="5" id="msg-badge">account_box</label>  
+      <div class="mdl-tooltip" for="msg-badge">Messages</div>                     
+
+      <label class="material-icons mdl-badge mdl-badge--overlap" data-badge="3" id="notif-badge">notifications</label> 
+      <div class="mdl-tooltip" for="notif-badge">Notifications</div>           
+                
+      <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
+        <label class="mdl-button mdl-js-button mdl-button--icon" for="fixed-header-drawer-exp">
+          <i class="material-icons">search</i>
+        </label>
+        <div class="mdl-textfield__expandable-holder">
+          <input class="mdl-textfield__input" type="text" name="sample" id="fixed-header-drawer-exp">
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
