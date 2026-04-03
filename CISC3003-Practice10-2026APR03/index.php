d7c538d91b507bdea39f12ea2bff211f743c2e95
WilliamLokUM <dc32568@umac.mo>
2026-04-03 18:23:29 +0800

    commit

---
+++ CISC3003-Practice10-2026APR03/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>DC325683 LOK WANG FONG's PHP</title>
+</head>
+<body>
+    <h1>LOK WANG FONG's PHP</h1>
+    
+    <p>
+        <?php
+        print "The SHA256 hash of \"LOK WANG FONG\" is ";
+        print hash('sha256', 'LOK WANG FONG');
+        ?>
+    </p>
+
+    <pre>ASCII ART:
+
+    **
+    **
+    **
+    **
+    **
+    **
+    **********
+    </pre>
+
+    <p>
+        <a href="fail.php">Click here to cause a traceback</a>
+    </p>
+    <p>
+        <a href="check.php">Click here to check the error setting</a>
+    </p>
+</body>
+</html>
